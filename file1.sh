bbf49b3840f294b0769e0b1ea7829f563c9dd455
ramesh <raghu.101997@gmail.com>
2023-02-01 08:27:44 -0800

    deleted the all the files

---
+++ file1.sh
@@ -0,0 +1,5 @@
+
+ls -lrt | wc -l
+
+top && pwd && free -m && touch demo3.sh
+
---
+++ text2.sh
@@ -0,0 +1,6 @@
+echo "please enter the file name"
+read filename
+touch $filename
+echo "please enter the directory name"
+read directoryname 
+mkdir $directoryname
